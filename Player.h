c48904251bd1a43e93bd4113f479bda916a16339
leavez529 <984199253@qq.com>
2017-05-16 20:30:00 +0800

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,13 @@
+class GameLevelLayer : public cocos2d::CCLayer
+{
+public:
+	GameLevelLayer(void) {};
+	~GameLevelLayer(void) {};
+	CREATE_FUNC(GameLevelLayer);
+
+	bool init();
+	static cocos2d::CCScene* scene();
+	SceneManager* smr;
+protected:
+	cocos2d::CCTMXTiledMap *map;
+};
