914b7458b82f5e50c1ae79584065ad097b8309e4
leavez529 <984199253@qq.com>
2017-06-14 15:30:41 +0800

    Create Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,12 +1,23 @@
 #include"Player.h"
+
 Player* Player::create(const char *pszFileName)
+
 {
+
 	Player *pobPlayer = new Player();
+
 	if (pobPlayer && pobPlayer->initWithFile(pszFileName))
+
 	{
+
 		pobPlayer->autorelease();
+
 		return pobPlayer;
+
 	}
+
 	CC_SAFE_DELETE(pobPlayer);
+
 	return NULL;
+
 }
