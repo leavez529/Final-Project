fbbe9c9e3d6c1a3ad3f7098ced8e48b8d0a26de5
leavez529 <984199253@qq.com>
2017-06-14 15:30:07 +0800

    Create MenuLayer.h

--- MenuLayer.h
+++ MenuLayer.h
@@ -1,13 +1,26 @@
+#pragma once
 #ifndef _MenuLayer_h_
+
 #define _MenuLayer_h_
+
 #include <cocos2d.h>
+
 USING_NS_CC;
 
+
+
 class MenuLayer :public Layer {
+
 public:
+
 	void menuCallback(Ref* pSender);
+
 	virtual bool init();
+
 	CREATE_FUNC(MenuLayer);
+
 	void toGameScene();
+	static Scene* createScene();
 };
+
 #endif // _MenuLayer_h_
